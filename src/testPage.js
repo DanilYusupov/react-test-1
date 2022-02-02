9641a5a1563b27b63ed493cff714ec1a7edc9639
Danil Yusupov <danilka5318rus@gmail.com>
2022-02-02 16:02:01 +0300

    2nd task done

--- src/testPage.js
+++ src/testPage.js
@@ -1,15 +1,36 @@
 import { useState } from "react"
+import { useStopwatch } from 'react-timer-hook';
 
 export const TestPage = () => {
 
     const [clicks, setClicks] = useState(0);
+    const [countDown, setCountDown] = useState(3);
+    const {
+        seconds,
+        restart,
+    } = useStopwatch
+
+    const handleClick = () => {
+        setCountDown(countDown - 1);
+        console.log(`CountDown: ${countDown}`);
+        if (seconds > 1) {
+            restart();
+        } else {
+            if (countDown <= 0) {
+                console.log('More than 3 clicks per second');
+                setCountDown(3);
+            } else {
+                setClicks(clicks + 1);
+            }
+        }
+    }
 
     return (
         <section>
             <p>
                 <span>{clicks}</span> cicks!
             </p>
-            <button onClick={() => setClicks(clicks + 1)}>
+            <button onClick={handleClick}>
                 Press me
             </button>
         </section>
